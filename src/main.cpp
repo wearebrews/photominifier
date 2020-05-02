8515cf493f658900bd1343562c364d0b182fa1dc
Håvard Skåra Mellbye <havardmellbye@gmail.com>
2020-05-02 17:17:15 +0200

    Add CURL

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdlib.h>
 #include "aws/core/Aws.h"
 #include "aws/core/auth/AWSCredentials.h"
 #include "aws/core/auth/AWSCredentialsProvider.h"
@@ -6,19 +7,20 @@
 #include "aws/s3/S3Client.h"
 #include "aws/s3/model/ListObjectsRequest.h"
 
+#include "curl/curl.h"
+
 extern "C" {
     #include <jpeglib.h>
 }
 
-const auto TOKEN = "JVLT6CB42I56VAU7DQWJ";
-const auto SECRET = "gfDWScllR9WAaKfOWbZs3OT4ZIFeKrdWmVaVeiiqJs4";
+const auto TOKEN = getenv("TOKEN");
+const auto SECRET = getenv("SECRET");
 const auto ENDPOINT_URL = "https://fra1.digitaloceanspaces.com";
 const auto CREDENTIALS = Aws::Auth::SimpleAWSCredentialsProvider(TOKEN, SECRET).GetAWSCredentials();
 
-int main(int argc, char** argv) {
+int main() {
     Aws::SDKOptions options;
     Aws::InitAPI(options);
-
     Aws::Client::ClientConfiguration client_config;
     client_config.endpointOverride = ENDPOINT_URL;
     auto s3_client = Aws::S3::S3Client(CREDENTIALS, client_config);  

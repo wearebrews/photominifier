bdd25f8100e9cef1bee5995a606b916ca3fe57d4
Håvard Skåra Mellbye <havardmellbye@gmail.com>
2020-09-15 13:10:22 +0200

    Use vcpkg as dependency manager

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <stdlib.h>
 #include "curl/curl.h"
-#include "photorepository.hpp"
 #include <memory>
 #include <fstream>
 #include "optimize_jpeg.hpp"
@@ -13,40 +12,6 @@ const std::string SECRET = [](){ auto s = getenv("SECRET"); return s ? s : "";}(
 
 
 int main() {
-    auto photos = rep.List("img/fullsize");
-    for (auto& p : photos) {
-        auto name = p.FileName();
-        std::cout << p.FileName() << std::endl;
-        if (name[name.length()-1] != '/') {
-            try {
-                auto buffer = rep.Download(p);
-                auto high = std::vector<unsigned char>(buffer.size());
-                auto medium = std::vector<unsigned char>(buffer.size());
-                auto low = std::vector<unsigned char>(buffer.size());
-                auto instagram = std::vector<unsigned char>(buffer.size());
-
-                auto optimize = [&buffer = std::as_const(buffer)](std::vector<unsigned char>& out, int quality = 75, int pixels = -1) {
-                    jpeg::optimize(buffer, out, quality, pixels);
-                };
-
-                std::thread t_high(optimize, std::ref(high));
-                std::thread t_medium(optimize, std::ref(medium), 75, 1920*1080);
-                std::thread t_low(optimize, std::ref(low), 50, 1000*1000);
-                std::thread t_instagram(optimize, std::ref(instagram), 50, 500*500);
-
-                t_high.join();
-                t_medium.join();
-                t_low.join();
-                t_instagram.join();
-                auto filename = [old = p.FileName()](std::string quality) {
-                    auto f = old;
-                    f.replace(f.find("fullsize"), std::string("fullsize").size(), quality);
-                    return f;
-                };
-            } catch(const std::exception& e) {
-                std::cout << "Error: " << e.what() << std::endl;
-            }
-        }
-    }
+    std::cout << "Hello world" << std::endl;
     return 0;
 }
